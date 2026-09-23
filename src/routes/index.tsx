a6cb3f386d7ed87b288727222e09262c8c164d7e
tcandra24 <tcandra007@gmail.com>
2026-09-23 12:24:44 +0700

    update

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,7 +5,7 @@ export const Route = createFileRoute('/')({ component: Home })
 function Home() {
   return (
     <div className="p-8">
-      <h1 className="text-4xl font-bold">Welcome</h1>
+      <h1 className="text-4xl font-bold">Welcome Friend</h1>
     </div>
   )
 }
